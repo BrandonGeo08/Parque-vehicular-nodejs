07cc55e28eef89ddb4b1f2357628e44d4cc76ba8
Brandon <brandongeo08@gmail.com>
2023-09-27 16:56:16 -0600

    navegacion/enpartials, metodo post

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const pool = require('../database')
+
+
 router.get('/add', (req, res) =>{
     res.render('links/add')
 });
 
+router.post('/add', async (req, res) =>{
+    const {clvEmpleado,sucursal,nomEmpleado,apPaterno,apMaterno,yearModelo,
+        modelo,colorMoto,numSerie,numPlacas,numPoliza,numInciso,polizaVencimiento,
+        numFactura,fechaFactura,subtotalFactura,ivaFactura,totalFactura,aseguradora,
+        estadoPoliza,observaciones} =  req.body;
+    
+    const newData = {
+        clvEmpleado,sucursal,nomEmpleado,apPaterno,apMaterno,yearModelo,
+        modelo,colorMoto,numSerie,numPlacas,numPoliza,numInciso,polizaVencimiento,
+        numFactura,fechaFactura,subtotalFactura,ivaFactura,totalFactura,aseguradora,
+        estadoPoliza,observaciones
+    }
+    await pool.query('INSERT INTO relacionvehiculos set ?', [newData])
+    res.render('links/add')
+});
 
 module.exports = router
