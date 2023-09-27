fb9a22451c252045741eb1d273288657058bab15
Brandon <brandongeo08@gmail.com>
2023-09-27 10:07:30 -0600

    Dependencias, Creacion de carpetas, Configuracion de index, Prueba de rutas

---
+++ notas.md
@@ -0,0 +1,13 @@
+express: framework de nodejs para creacion de backend 
+express-handlebars | Ejs: motores de plantillas 
+express-session: administra las sessiones de nuestra app
+mysql: modulo para conectar y consultas a base de datos 
+express-mysql-session: almacena las sessiones 
+morgan: permite crear logs o mensajes de lo que la app pide al servidor 
+bcryptjs: cifra las contraseñas de usuarios antes de guardarlas
+passport: modulo para autenticar y manejar el proceso de login 
+passport-local: autenticar usuarios con nuestra db
+timeago.js: convierte los timestamps o fechas en formato de: 2 minutes ago, 2 hours ago
+connect-flash: muestra mensajes de error y exito cuando usuario realice operación 
+express-validator: valida los datos que el usuario nos envia desde el cliente 
+nodemon: 
---
+++ src/index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+/**
+ * Initializations
+ */
+const app = express();
+
+/**
+ * settings
+ */
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+  }))
+app.set('view engine', '.hbs');
+
+/**
+ * Middlewares
+ */
+app.use(morgan('dev'));
+app.use(express.urlencoded({extends: false}));
+app.use(express.json());
+
+/**
+ * Global Variables 
+ */
+app.use((req, res, next)=>{
+    
+    next();
+})
+
+/**
+ * Routes
+ */
+app.use(require('./routes/index'));
+app.use(require('./routes/authentication'))
+app.use(require('./routes/authentication'))
+
+
+/**
+ * Public
+ */
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/**
+ * Starting the server
+ */
+app.listen(app.get('port'), () =>{
+    console.log('Server on port', app.get('port'));
+});
